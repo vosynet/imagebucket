7b017e3e878fd8828e9047130c1a2bc8c13b79d7
Hendro <hdrxs312@gmail.com>
2020-09-11 14:43:13 +0700

    remove firebase functions

--- helper.js
+++ helper.js
@@ -1,9 +1,22 @@
 'use strict'
 
 const fs = require('fs')
-const axios = require('axios')
 const sharp = require('sharp')
+const moment = require('moment')
 const tmpPath = './tmp/'
+const admin = require('firebase-admin')
+const serviceAccount = require('./service.json')
+const projectId = serviceAccount.project_id
+const config = {
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: `https://${projectId}.firebaseio.com`,
+    projectId: projectId,
+    storageBucket: `${projectId}.appspot.com`,
+    cdnDomain: `https://storage.googleapis.com/${projectId}.appspot.com`,
+    projectHost: `https://${projectId}.web.app`
+}
+const firebase = admin.initializeApp(config)
+const bucket = firebase.storage().bucket()
 
 class Helper {
     async resizeImage(path, size) {
@@ -32,22 +45,14 @@ class Helper {
             if (!resizing) return false
         }
 
-        let bitmap = fs.readFileSync(tmpFilePath)
-        let imageData = Buffer.from(bitmap).toString('base64')
-        let result = await axios({
-            url: req.firebaseHost + '/upload',
-            method: 'post',
-            data: {
-                filename: fileName,
-                image: imageData
-            }
-        })
-        .then(({ data}) => {
-            return data.url
+        let date = moment().format('YYYYMMDD')
+        await bucket.upload(tmpFilePath, {
+            destination: date + '/' + fileName,
+            public: true,
         })
 
         fs.unlinkSync(tmpFilePath)
-        return result
+        return `${config.cdnDomain}/${date}/${fileName}`
     }
 }
 
--- index.js
+++ index.js
@@ -9,23 +9,10 @@ const fs = require('fs')
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 4000
 
-try {
-	const serviceAccount = require('./firebase/functions/service.json')
-	process.env.FIREBASE_HOST = `https://${serviceAccount.project_id}.web.app`
-	process.env.PRIVATE_KEY = serviceAccount.private_key_id
-}
-catch(err){
-	throw new Error('service.json was invalid')
-}	
-
 if (!fs.existsSync('./tmp')) fs.mkdirSync('./tmp')
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(function(req, res, next) {
-	req.firebaseHost = process.env.FIREBASE_HOST
-	next()
-})
 
 app.get('/', ctrl.index)
 app.post('/upload/json', ctrl.uploadJson)
